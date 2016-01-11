2e9e29e89f4ed6b7ea710979bbb54bf9668c3fcd
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-11 13:33:55 -0800

    Build pike object, calc custHr

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-//my JS will go here
+//my JS will go here//
+
+
+//building my pikeplace obect//
+var pike = {
+  minHr: 14,
+  maxHr: 55,
+  cupCust: 1.2,
+  lbCust: 3.7,
+  custHour: function(minHr, maxHr) {
+    return Math.floor(Math.random() * (this.maxHr - this.minHr +1)) + this.minHr
+  }
+}
