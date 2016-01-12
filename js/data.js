abb8060cbd9d842554919537af16b9fa0037af09
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-12 15:24:03 -0800

    first hour displays in toTables

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 //my JS will go here//
 var allHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm']
+var allStores = [];
 
 //Building my constructor
 var Store = function(storeName, minHr, maxHr, cupCust, lbCust){
@@ -14,6 +15,7 @@ var Store = function(storeName, minHr, maxHr, cupCust, lbCust){
   this.lbCust = lbCust;
   this.dailyCups = 0;
   this.dailyBeans = 0;
+  allStores.push(this);
 
   this.custHour = function() {
     for (var i = 0; i < this.hours.length; i ++) {
@@ -60,9 +62,55 @@ var slu = new Store('South Lake Union', 35, 88, 1.3, 3.7);
 var seaTac = new Store('Sea-Tac Airport', 68, 124, 1.1, 2.7);
 var website = new Store('Website Sales', 3, 6, 0, 6.7);
 
-var allStores = [pike, capHill, spl, slu, seaTac, website];
 
-//populate to the webpage
-for (var i = 0; i < allStores.length; i++) {
-  allStores[i].populate();
-};
+// //populate to the webpage
+// for (var i = 0; i < allStores.length; i++) {
+//   allStores[i].populate();
+// };
+
+
+var toTables = function () {
+  //calculate data
+  for (var i = 0; i < allStores.length; i++) {
+    allStores[i].custHour();
+    allStores[i].cupsPerHr();
+    allStores[i].beanHr();
+  }
+  //make the title
+  var h2El = document.createElement('h2');
+  h2El.textContent = 'Bean Forecast Data';
+  document.body.appendChild(h2El);
+  //make the table
+  var tableEl = document.createElement('table');
+
+
+  //display the header row of the table
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = ' ';
+  trEl.appendChild(thEl);
+  tableEl.appendChild(trEl);
+
+  for (var i = 0; i < allStores.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = allStores[i].storeName;
+    trEl.appendChild(thEl);
+  }
+
+  //display one row of regular data
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = allHours[0];
+  trEl.appendChild(thEl);
+  for (var j = 0; j < (allStores.length); j++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allStores[j].beansEachHour[0].toFixed(1);
+    trEl.appendChild(tdEl)
+  }
+  tableEl.appendChild(trEl);
+
+
+  document.body.appendChild(tableEl);
+}
+
+toTables();
